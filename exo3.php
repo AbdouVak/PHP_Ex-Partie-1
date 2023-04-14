fce973f31f72eca2a7796bab89f3c03479456946
AbdouVakgit config --global user.name AbdouVak <abdourahman.vakouiev@gmail.com>
2023-04-14 13:42:31 +0200

    Exo3: str_replace remplacer un mot par un autre

---
+++ exo3.php
@@ -0,0 +1,12 @@
+<h1>Exercice 3</h1>
+
+<p>Soit la phrase notre "Notre formation DL commence aujourd'hui".<br>
+Ecrire un algorithme permetant de compter le nombre de caracter contenus dans cette phrase</p>
+
+<h2>Résultat</h2>
+
+<?PHP
+
+$phrase = "Notre formation DL commence aujourd'hui";
+echo str_replace("aujourd'hui","demain",$phrase);
+?>
