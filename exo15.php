2d60f3e5766ca22ccf0dc08ef04cc91e2a734e5b
AbdouVakgit config --global user.name AbdouVak <abdourahman.vakouiev@gmail.com>
2023-04-14 14:54:53 +0200

    amelioration du code avec set get pour chaque info + fonction pour calculer l'age

--- exo15.php
+++ exo15.php
@@ -6,34 +6,55 @@ Instancier 2 personnes et afficher leurs informations : nom, prénom et âge.</p
 <h2>Résultat</h2>
 
 <?PHP
-Class personne{
+Class Personne{
     
-    private $_nom;
-    private $_prenom;
-    private $_age;
+    private string $_nom;
+    private string $_prenom;
+    private DateTime $_dateNaissance;
 
-    public function __construct($nom,$prenom,$date_naissance){
-
+    public function __construct(string $nom, string $prenom, string $dateNaissance){
         $this->_nom = $nom;
         $this->_prenom = $prenom;
-
-        $date_naissance_DT = new DateTime($date_naissance);
-        $date_actuel = new DateTime('now');
-        $difference = $date_naissance_DT->diff($date_actuel);
-        $this->_age = $difference->format('%y');
-
+        $this->_dateNaissance = new DateTime($dateNaissance);
     }
-    
-    public function afficher_age(){
-        echo " $this->_prenom $this->_nom a $this->_age ans<br>";
+
+    public function getNom() : string{
+        return $this->_nom;
+    }
+    public function setNom(string $nom){
+        $this->_nom =$nom;
+    }
+
+    public function getPrenom() : string{
+        return $this->_prenom;
+    }
+    public function setPrenom(string $prenom){
+        $this->_prenom =$prenom;
+    }
+
+    public function getDateNaisssance() : DateTime{
+        return $this->_dateNaissance;
+    }
+    public function setDateNaisssance(DateTime $dateNaissance){
+        $this->_dateNaissance = $dateNaissance;
+    }
+
+    public function calculerAge(){
+        $dateActuel = new DateTime('now');
+        $difference = $this->_dateNaissance->diff($dateActuel);
+        return $difference->format('%y');
+    }
+
+    public function __toString(){
+        return "$this->_prenom $this->_nom a ".$this->calculerAge()." ans <br>";
     }
 
 }
 
-$p1 = new personne("DUPONT","Michel","1980-02-19");
+$p1 = new Personne("DUPONT","Michel","1980-02-19");
 $p2 = new Personne("DUCHEMIN", "Alice", "1985-01-17") ;
 
-echo $p1->afficher_age();
-echo $p2->afficher_age();
+echo $p1;
+echo $p2;
 
 ?>
